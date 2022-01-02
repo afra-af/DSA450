19cf57108ae0c64de4601ba634e276d12544620f
AFRA <81957946+afra-af@users.noreply.github.com>
2022-01-02 19:33:05 +0530

    Create reversestring.java

---
+++ string/reversestring.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseString(char[] s) {
+        char temp;
+        int n=s.length;
+        for(int i=0;i<=(s.length-1)/2;i++){
+            temp=s[i];
+            s[i]=s[n-i-1];
+            s[n-i-1]=temp;
+}
+    }
+}
