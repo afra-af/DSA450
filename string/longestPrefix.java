9d1e2869382614395b3043e63f8dc95680d1eda5
AFRA <81957946+afra-af@users.noreply.github.com>
2022-07-02 20:41:34 +0530

    Create longestPrefix.java

---
+++ string/longestPrefix.java
@@ -0,0 +1,47 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+
+*/
+import java.util.*;
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder result = new StringBuilder();
+
+    // Sort the array
+    Arrays.sort(strs);
+
+    // Get the first and last strings and convert it to char as to do iteration on them
+    char[] first = strs[0].toCharArray();
+    char[] last = strs[strs.length - 1].toCharArray();
+
+    // Start comparing each char in first and last 
+    for (int i = 0; i < first.length; i++) {
+      if (first[i] != last[i])
+        break;
+      result.append(first[i]);
+    }
+
+    return result.toString();
+  }
+}
