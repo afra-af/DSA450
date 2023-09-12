2801e86cec4ee7e2ee5247223f147891b96076b5
AFRA <81957946+afra-af@users.noreply.github.com>
2023-09-12 13:54:36 +0530

    LinkedList in java collections

---
+++ LinkedList/LLCollection.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class LLCollection{
+  LinkedList<String> LL=new LinkedList<>();
+  LL.addFirst("this");
+  LL.addLast("is");
+  //by default adds at last
+  LL.add("a");
+  LL.size();
+  LL.removeFirst();
+  LL.removeLast():
+  for(int i=0;i<size();i++){
+  System.out.print(LL.get(i)+"->");
+  }
+System.out.print("Null");
+}
+
