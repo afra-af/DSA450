89fd2fa59b93560005a8731125e41ca149a0c18c
AFRA <81957946+afra-af@users.noreply.github.com>
2023-12-30 17:25:36 +0530

    Create ifelse.java

---
+++ basics/ifelse.java
@@ -0,0 +1,43 @@
+/**
+Problem statement
+Programming languages have some conditional / decision-making statements that execute when some specific condition is fulfilled.
+
+
+
+If-else is one of the ways to implement them.
+
+
+
+You are given two numbers 'a' and 'b'.
+
+
+
+Compare the numbers and print the relation.
+
+
+
+Print “smaller”, “greater” or “equal” when ‘a’ is smaller than ‘b’, greater than ‘b’, or equal to ‘b’ respectively.
+
+
+
+Example :
+Input: ‘a’ = 5 and ‘b’ = 3
+
+Output: greater
+
+*/
+public class Solution {
+    public static String compareIfElse(int a, int b) {
+        
+        if(a>b){
+             return "greater";
+        }
+        else if(a==b){
+             return "equal";
+        }
+        else{
+             return "smaller";
+        }
+
+    }
+}
