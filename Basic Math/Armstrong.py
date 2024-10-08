e431d42dfd43d016eb9440e9ab8583c6ead751b6
AFRA <81957946+afra-af@users.noreply.github.com>
2024-10-08 16:57:20 +0530

    Create Armstrong.py

---
+++ Basic Math/Armstrong.py
@@ -0,0 +1,33 @@
+'''
+You are given a 3-digit number n, Find whether it is an Armstrong number or not.
+
+An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371. 
+
+Note: Return "true" if it is an Armstrong number else return "false".
+
+Examples
+
+Input: n = 153
+Output: true
+Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153. Hence answer is "true".
+Input: n = 372
+Output: false
+Explanation: 372 is not an Armstrong number since 33 + 73 + 23 = 378. Hence answer is "false".
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1) 
+
+Constraints:
+100 ≤ n <1000 
+'''
+class Solution:
+    def armstrongNumber (self, n):
+        # code here
+        sum=0
+        original_n=n
+        while n>0:
+            temp=n%10
+            sum+=temp**3
+            n=n//10
+        if sum==original_n:
+            return "true"
+        return "false"
